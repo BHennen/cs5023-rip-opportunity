3c40536c1e43f8f68076b5873708f058ac1ed726
Bryce Hennen <ecyrbnenneh@gmail.com>
2019-04-23 17:08:32 -0500

    grammar

--- src/speech_commands/speech_commands.py
+++ src/speech_commands/speech_commands.py
@@ -5,6 +5,7 @@ class SpeechCommands():
     def __init__(self):
         self.st = SpeechTranscriber()
         self.st.start_listening(self.handle_that_audio)
-    
+        
+
     def handle_that_audio(self, speech):
         pass
--- src/speech_commands/speech_transcriber.py
+++ src/speech_commands/speech_transcriber.py
@@ -2,7 +2,36 @@ import speech_recognition as sr
 
 class SpeechTranscriber():
     def __init__(self):
-        pass
+        self.r = sr.Recognizer()
+        self.m = sr.Microphone()
 
     def start_listening(self, cb):
-        pass
+        try:
+            print("A moment of silence, please...")
+            with self.m as source:
+                self.r.adjust_for_ambient_noise(source)
+            print("Set minimum energy threshold to {}".format(self.r.energy_threshold))
+            while True:
+                print("Say something!")
+                with self.m as source:
+                    audio = self.r.listen(source, timeout=3.0, phrase_time_limit=1.0)
+                print("Got it! Now to recognize it...")
+                try:
+                    # recognize speech using Google Speech Recognition
+                    value = self.r.recognize_sphinx(
+                        audio_data=audio, grammar='grammar.gram')
+
+                    # we need some special handling here to correctly print unicode characters to standard output
+                    # this version of Python uses bytes for strings (Python 2)
+                    if str is bytes:
+                        print(u"You said {}".format(value).encode("utf-8"))
+                    # this version of Python uses unicode for strings (Python 3+)
+                    else:
+                        print("You said {}".format(value))
+                except sr.UnknownValueError:
+                    print("Oops! Didn't catch that")
+                except sr.RequestError as e:
+                    print(
+                        "Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+        except KeyboardInterrupt:
+            pass
