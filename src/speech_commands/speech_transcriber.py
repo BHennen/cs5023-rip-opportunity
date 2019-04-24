2cbd7318cf863b7e19a4e5570d92b73aa2bc3496
Bryce Hennen <ecyrbnenneh@gmail.com>
2019-04-24 00:43:03 -0500

    restructured, merged grammar and keyword branches

--- src/speech_commands/speech_transcriber.py
+++ src/speech_commands/speech_transcriber.py
@@ -1,25 +1,36 @@
 import speech_recognition as sr
 
+
 class SpeechTranscriber():
     def __init__(self):
         self.r = sr.Recognizer()
         self.m = sr.Microphone()
+        print("A moment of silence, please...")
+        with self.m as source:
+            self.r.adjust_for_ambient_noise(source)
+        print("Set minimum energy threshold to {}".format(self.r.energy_threshold))
 
     def start_listening(self, cb):
+        #TODO: clean method of listening for hotword then for grammar in loop
+        pass
+
+    def listen(self, grammar=None, keyword_entries=None):
+        if grammar is None and keyword_entries is None:
+            raise Exception(
+                "Must specify grammar file path or keyword_entries iterable of tuples.")
         try:
-            print("A moment of silence, please...")
-            with self.m as source:
-                self.r.adjust_for_ambient_noise(source)
-            print("Set minimum energy threshold to {}".format(self.r.energy_threshold))
             while True:
                 print("Say something!")
                 with self.m as source:
                     audio = self.r.listen(source, phrase_time_limit=3.0)
                 print("Got it! Now to recognize it...")
                 try:
-                    # recognize speech using Google Speech Recognition
-                    value = self.r.recognize_sphinx(
-                        audio_data=audio, grammar='src\speech_commands\commands.gram')
+                    if grammar:
+                        value = self.r.recognize_sphinx(
+                            audio_data=audio, grammar=grammar)
+                    elif keyword_entries:
+                        value = self.r.recognize_sphinx(
+                            audio_data=audio, keyword_entries=keyword_entries)
 
                     # we need some special handling here to correctly print unicode characters to standard output
                     # this version of Python uses bytes for strings (Python 2)
@@ -36,5 +47,53 @@ class SpeechTranscriber():
         except KeyboardInterrupt:
             pass
 
-sc = SpeechTranscriber()
-sc.start_listening('123')
+if __name__ == "__main__":
+    import sys
+    import os
+
+    if len(sys.argv) > 1:
+        test = sys.argv[1]
+    else:
+        test = 'keywords'
+
+    data_path = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), "data")
+    st = SpeechTranscriber()
+    print(test)
+
+    if test == 'keywords':
+        # run keywords/hotwords test
+        keywords = []
+        # sensitivity = 1  # best you can do without editing line 769 of __init__.py of speech_recognition library
+        # or you can hack some stuff; this will make sensitivity 1e[hack_value]
+        # doing this to avoid editing their module
+        class Hack:
+            def __init__(self, val):
+                self.val = val
+
+            def __sub__(self, o):
+                return self
+
+            __rsub__ = __sub__
+            __mul__ = __sub__
+            __rmul__ = __sub__
+
+            def __str__(self):
+                return str(self.val)
+
+            def __repr__(self):
+                return '1e{}'.format(self)
+
+            def __le__(self, o):
+                return True
+                
+        sensitivity = Hack(-1) 
+        with open(os.path.join(data_path, "keywords.txt"), 'r') as f:
+            keywords = [(keyword.strip(), sensitivity)
+                        for keyword in f.readlines()]
+            print(keywords)
+
+        st.listen(keyword_entries=keywords)
+
+    elif test == 'grammar':
+        st.listen(os.path.join(data_path, "commands.gram"))
